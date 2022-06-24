d6b55228709dbdbef75f3fff957337d6f17831fa
胡晨旖 <1599647051@qq.com>
2022-06-24 22:54:07 +0800

    feat: update pattern summary

---
+++ DesignPatterns/PatternSummary.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+// MARK: - 创造型模式: 提供创建对象的机制，增加已有代码的灵活性和可复用性
+/// 1.工厂方法 FactoryMethod:
+/// 2.抽象工厂 AbstractFactory:
+/// 3.生成器 Bulider: 分步骤创建复杂对象，用于init参数过多或swift中的方法链和高级函数，创建者中的方法可返回自身或者返回创建的产品
+/// 4.原型 Prototype: 复制已有对象，swift中的NSCoping接口，个人理解是可做到深拷贝的引用类型class
+/// 5.单例 Singleton: 一个类只有一个实例，关键代码是提供一个访问该实例的全局静态属性static，需要注意并发情况
+
+
+// MARK: - 结构型模式: 介绍如何将对象和类组装成较大的结构，并同时保持结构的灵活和高效
+/// 1.适配器 Adapter:
+/// 2.桥接 Bridge:
+/// 3.装饰 Decorator:
+/// 4.外观 Facade:
+/// 5.代理 Proxy:
+/// 6.组合 Composite:  将对象组合成树状结构，递归
+/// 7.享元 Flyweight:  建立缓存池，如果已有相应的享元就直接复用，没有找到就新建并添加到缓存池中。类似collectionViewCell的复用
+
+// MARK: - 行为模式: 负责对象间高效沟通和职责委派
+
+
+
